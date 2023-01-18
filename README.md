9a0b5b6ea0cccba264ab7be79502dc43717641c1
Daniil Shulgin <daniqshu@gmail.com>
2023-01-18 18:03:28 +0300

    No companies found fixes, readme link fix

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Run this commands to start a project
 - `flutter pub get`
 - `pub run build_runner build --delete-conflicting-outputs`
 - And then add your api key in **/lib/api/stock_api_constant.dart**, there is no flavors/env in this project, so you have to do it manually like this.
-  If you dont have one yet, then create it here - alphavantage.co
+  If you dont have one yet, then create it here -[alphavantage.co](https://www.alphavantage.co/support/#api-key)
--- lib/ui/pages/overview_page.dart
+++ lib/ui/pages/overview_page.dart
@@ -26,35 +26,56 @@ class _OverviewPageState extends State<OverviewPage> {
           builder: (context, state) {
             return state.maybeWhen(
               loaded: (companiesOverview) {
-                final double totalMarketCap = companiesOverview.fold(
-                  0,
-                  (previousValue, element) =>
-                      previousValue + (element.marketCapitalization ?? 0),
-                );
+                if (companiesOverview.isNotEmpty) {
+                  final double totalMarketCap = companiesOverview.fold(
+                    0,
+                    (previousValue, element) =>
+                        previousValue + (element.marketCapitalization ?? 0),
+                  );
 
-                return CompaniesLoadedWidget(
-                  totalMarketCap: totalMarketCap,
-                  companiesOverviewUi: List.generate(
-                    companiesOverview.length,
-                    (index) {
-                      final backgroundColor =
-                          Color((Random().nextDouble() * 0xFFFFFF).toInt())
-                              .withOpacity(1);
+                  return CompaniesLoadedWidget(
+                    totalMarketCap: totalMarketCap,
+                    companiesOverviewUi: List.generate(
+                      companiesOverview.length,
+                      (index) {
+                        final backgroundColor =
+                            Color((Random().nextDouble() * 0xFFFFFF).toInt())
+                                .withOpacity(1);
 
-                      return CompanyOverviewUi(
-                        backgroundColor: backgroundColor,
-                        companyOverview: companiesOverview[index],
-                        marketCapPercentage:
-                            ((companiesOverview[index].marketCapitalization ??
-                                        0) *
-                                    100 /
-                                    totalMarketCap)
-                                .toStringAsFixed(2),
-                        textColor: backgroundColor.computeLuminance() > 0.5
-                            ? Colors.black
-                            : Colors.white,
-                      );
-                    },
+                        return CompanyOverviewUi(
+                          backgroundColor: backgroundColor,
+                          companyOverview: companiesOverview[index],
+                          marketCapPercentage:
+                              ((companiesOverview[index].marketCapitalization ??
+                                          0) *
+                                      100 /
+                                      totalMarketCap)
+                                  .toStringAsFixed(2),
+                          textColor: backgroundColor.computeLuminance() > 0.5
+                              ? Colors.black
+                              : Colors.white,
+                        );
+                      },
+                    ),
+                  );
+                }
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Компании не были получены\nПроверьте подключние к интеренету и свой api key\n(читать READEME.MD)',
+                        textAlign: TextAlign.center,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          getIt<StockCubit>().fetchCompaniesOverview(
+                            companySymbols: selectedCompanies,
+                          );
+                        },
+                        child: const Text('Повторить загрузку'),
+                      ),
+                    ],
                   ),
                 );
               },
